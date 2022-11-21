897735d1b4847081cd8f7243db90713006a06751
Dimitar Slaev <dimitarslaev@Macs-MacBook-Pro.local>
2022-11-21 23:39:47 +0200

    add items js

--- src/app.js
+++ src/app.js
@@ -1,62 +1,37 @@
-gsap.registerPlugin(ScrollTrigger);
-// gsap.to(window, { duration: 2, scrollTo: 10 });
+import data from "./data.json";
+import itemsjsPackage from "itemsjs";
 
-const items = gsap.utils.toArray("#container > *");
-const itemWidth = items[0].querySelector(".circle").offsetWidth;
-const timeline = gsap.timeline({ paused: true });
-
-const createItemTl = (item) => {
-  return gsap.fromTo(
-    item,
-    1,
-    {
-      scale: 0.38,
-      x: "-20.75%",
+const itemsjs = itemsjsPackage(data, {
+  sortings: {
+    title_asc: {
+      field: "title",
+      order: "asc",
     },
-    {
-      scale: 1,
-      x: 0,
-      yoyo: true,
-      repeat: 1,
-    }
-  );
-};
-
-items.forEach((item, index) => {
-  const tl = createItemTl(item);
-  timeline.add(tl).addLabel("tl-" + index);
+  },
+  aggregations: {
+    tags: {
+      name: "Tags",
+      size: 10,
+      conjunction: false,
+    },
+  },
+  searchableFields: ["name", "tags"],
 });
 
-const trigger = ScrollTrigger.create({
-  animation: timeline,
-  trigger: document.body,
-  start: "top top",
-  end: "bottom bottom",
-  scrub: true,
-  onUpdate: (self) => {},
+const articles = itemsjs.search({
+  per_page: 4,
+  sort: "title_asc",
+  // full text search
+  // query: 'forrest gump',
+  // filters: {
+  //   tags: ["tag 1"],
+  // },
 });
-console.log(timeline.getChildren());
-const appendSection = () => {
-  const firstPost = document.querySelector(".post:first-child");
-  const lastPost = document.querySelector(".post:last-child");
 
-  const el = document.createElement("div");
-  el.classList.add("post");
-  el.innerHTML = lastPost.innerHTML;
-
-  firstPost.after(el);
-
-  const tl = createItemTl(el);
-
-  timeline.shiftChildren(1, false, 1);
-  timeline.add(tl, 1);
-
-  trigger.refresh();
-  gsap.to(window, {
-    duration: 1,
-    scrollTo: { y: el.offsetTop },
-    overwrite: "auto",
-  });
-};
-
-document.querySelector("button").addEventListener("click", appendSection);
+/**
+ * get list of top tags
+ */
+const top_tags = itemsjs.aggregation({
+  name: "tags",
+  per_page: 10,
+});
